9e471910ee4a5b68d07ff2ca7889cbb9914ab910
josephbabs <bajos3d@gmail.com>
2023-09-15 19:15:51 +0100

    images updated

--- components/GoogleMap.vue
+++ components/GoogleMap.vue
@@ -1,11 +1,14 @@
 <template>
   <!--Contact Page Google Map Start-->
   <section class="contact-page-google-map">
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4562.753041141002!2d-118.80123790098536!3d34.152323469614075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82469c2162619%3A0xba03efb7998eef6d!2sCostco+Wholesale!5e0!3m2!1sbn!2sbd!4v1562518641290!5m2!1sbn!2sbd"
+    <!-- <iframe
+      src="https://maps.app.goo.gl/ZyFXabhkcei3mJpD9"
       class="contact-page-google-map__one"
       allowfullscreen
-    ></iframe>
+    ></iframe> -->
+    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1178.903716651101!2d2.3831396062632835!3d6.349534215571204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1suemoa%20b%C3%A9nin!5e0!3m2!1sfr!2sbj!4v1694774460941!5m2!1sfr!2sbj" 
+    class="contact-page-google-map__one"
+      allowfullscreen style="border:0;"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </section>
   <!--Contact Page Google Map End-->
 </template>
